43328258ae19324c12632b722b884183bee5aa44
Gustavo <gustavo7dias@gmail.com>
2024-06-03 12:09:37 -0300

    Update format and add subject list

--- src/pages/RandomQuestion.vue
+++ src/pages/RandomQuestion.vue
@@ -1,5 +1,6 @@
 <script setup>
-import questions from "../mock/template-questions";
+import bellSound from "../assets/sound/copper-bell-ding-4-204990.mp3";
+import subjects from "../mock/subjects";
 </script>
 <template>
   <div class="global-background" id="random-words">
@@ -7,7 +8,16 @@ import questions from "../mock/template-questions";
       <div class="box-container">
         <form class="form" @submit.prevent="">
           <div class="row">
-            <h1 class="main-title">Random Question</h1>
+            <div class="scoreboard">
+              <div class="score">
+                <h3>Score</h3>
+                <p v-text="score"></p>
+              </div>
+              <div class="remaining">
+                <h3>Remaining</h3>
+                <p v-text="remaining"></p>
+              </div>
+            </div>
           </div>
           <div class="row">
             <h2 class="random-words" v-text="randomWord"></h2>
@@ -24,25 +34,23 @@ import questions from "../mock/template-questions";
           </div>
           <div class="row">
             <button class="gn-button pm-button full" @click="handleClick">
-              {{ buttonText }}
+              {{ buttonText }} (Enter)
             </button>
           </div>
           <div class="row">
-            <div class="input-container">
-              <input
-                id="seconds"
-                v-model="seconds"
-                type="text"
-                class="input filled"
-                :class="{ active: seconds.length > 0 }"
-              />
-              <label for="seconds" class="label">Seconds</label>
-              <div class="right-element">
-                <button class="gn-button sc-button full" @click="resetTime">
-                  Reset
-                </button>
-              </div>
-            </div>
+            <button class="gn-button pm-button full" @click="setCount">
+              Count (K)
+            </button>
+          </div>
+          <div class="row">
+            <input
+              id="seconds"
+              v-model.number="seconds"
+              type="text"
+              class="input filled"
+              :class="active(seconds)"
+            />
+            <label for="seconds" class="label">Seconds</label>
           </div>
         </form>
       </div>
@@ -51,37 +59,58 @@ import questions from "../mock/template-questions";
   </div>
 </template>
 
+<style scoped lang="scss">
+@import "../assets/styles/pages/random-words.scss";
+</style>
+
 <script>
 export default {
   data() {
     return {
-      list: questions,
-      seconds: "5",
-      randomWord: "",
+      isRunning: false,
+      list: [...subjects],
+      seconds: 0,
+      randomWord: "???",
       intervalRef: 0,
       showBar: false,
       toggleReflow: false,
+      score: 0,
+      remaining: 30,
     };
   },
   methods: {
     startRandom() {
-      this.stopRandom();
+      this.isRunning = true;
       this.choiceWord();
-      if (Number(this.seconds) > 0) {
-        this.intervalRef = setInterval(() => {
-          this.choiceWord();
-          this.toggle();
-          this.reflow();
-        }, Number(this.seconds) * 1000);
-      }
+
+      this.intervalRef = setInterval(() => {
+        this.next();
+      }, Number(this.seconds) * 1000);
     },
     stopRandom() {
       clearInterval(this.intervalRef);
+      this.isRunning = false;
+      this.remaining = 30;
+      this.randomWord = "???";
+    },
+    setCount() {
+      if (this.isRunning && this.score < 30) {
+        clearInterval(this.intervalRef);
+        this.score++;
+        this.next();
+        this.startRandom();
+        this.playSound();
+      }
+    },
+    next() {
+      this.remaining--;
+      this.choiceWord();
+      this.toggle();
+      this.reflow();
     },
     handleClick() {
-      if (Number(this.seconds) === 0) {
-        this.startRandom();
-      } else if (!this.showBar) {
+      if (Number(this.seconds) === 0) return;
+      if (!this.showBar) {
         this.showBar = !this.showBar;
         this.startRandom();
       } else {
@@ -90,8 +119,12 @@ export default {
       }
     },
     choiceWord() {
+      if (this.list.length === 0) {
+        this.list = this.shuffleArray([...subjects]);
+      }
       const letterIndex = Math.floor(Math.random() * this.list.length);
       const word = this.list[letterIndex];
+      this.list.splice(letterIndex, 1);
       this.randomWord = word;
     },
     reflow() {
@@ -103,24 +136,60 @@ export default {
     toggle() {
       this.toggleReflow = !this.toggleReflow;
     },
-    resetTime() {
-      this.seconds = "0";
+    shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    },
+    saveLocal(name, data) {
+      window.localStorage.setItem(name, data);
+    },
+    getLocal(name) {
+      return window.localStorage.getItem(name) || "8";
+    },
+    active(value = 0) {
+      return { active: value !== "" };
+    },
+    playSound() {
+      const audio = new Audio(bellSound);
+      audio.play();
     },
   },
   watch: {
+    remaining() {
+      if (this.remaining === 0) {
+        this.stopRandom();
+        this.showBar = false;
+      }
+    },
     seconds() {
       this.showBar = false;
       this.stopRandom();
+      this.saveLocal("seconds", this.seconds);
+    },
+    isRunning() {
+      if (this.isRunning === true) this.score = 0;
     },
   },
   computed: {
     buttonText() {
-      if (Number(this.seconds) > 0) {
-        return this.showBar ? "Stop" : "Start";
-      } else {
-        return "Next";
-      }
+      return this.showBar ? "Stop" : "Start";
     },
+  },
+  created() {
+    window.addEventListener("keypress", (e) => {
+      if (e.key == "k" || e.key == "K") {
+        this.setCount();
+      }
+    });
+    window.addEventListener("keypress", (e) => {
+      if (e.key == "Enter") {
+        this.handleClick();
+      }
+    });
+    this.seconds = Number(this.getLocal("seconds"));
   },
 };
 </script>
